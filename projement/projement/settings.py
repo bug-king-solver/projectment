ace13983bf99172c56966a6bdf6d9a43464f11e0
leo <janny.dream.high@gmail.com>
2023-09-23 16:59:26 +0900

    feat(task6): modify settings.ps

--- projement/projement/settings.py
+++ projement/projement/settings.py
@@ -121,6 +121,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,  # Specify the number of items per page
+}
+
 
 # Static site url, used when we need absolute url but lack request object, e.g. in email sending.
 SITE_URL = "http://127.0.0.1:8000"
